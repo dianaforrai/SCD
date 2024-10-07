2bcbcfbc2ad6cd1b47b25aa61636b3bb22728bc5
Diana Forrai Susana <diana.forrai12@yahoo.com>
2024-10-07 13:09:30 +0300

    Lab 1 comunicare client-server cu socket.

---
+++ SCDLab1/src/ClientSimplu.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientSimplu {
+    public static void main(String[] args)throws Exception{
+        Socket socket=null;
+        try {
+
+//creare obiect address care identifică adresa serverului
+            InetAddress address =InetAddress.getByName("localhost");
+            //se putea utiliza varianta alternativă: InetAddress.getByName("127.0.0.1")
+            socket = new Socket(address,1900);
+            BufferedReader in =new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+            // Output is automatically flushed  by PrintWriter:
+            PrintWriter out =new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())),true);
+            Scanner scanner = new Scanner(System.in);
+            String input = "";
+            while (!input.equals("END")) {
+                System.out.print(">");
+                input = scanner.next();
+                out.println("c: " + input);
+                String str = in.readLine();       //trimite mesaj
+                System.out.println(str);         //aşteaptă răspuns
+            }
+            /*for(int i = 0; i < 10; i ++) {
+                out.println("mesaj " + i);
+                String str = in.readLine();       //trimite mesaj
+                System.out.println(str);         //aşteaptă răspuns
+
+            } */
+            out.println("END");        //trimite mesaj care determină serverul să închidă conexiunea
+
+        }
+
+        catch (Exception ex) {ex.printStackTrace();}
+
+        finally{
+
+            socket.close();
+        }
+
+    }
+
+}
---
+++ SCDLab1/src/Server.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+public static void main(String[] args) throws IOException {
+
+ServerSocket ss=null;
+Socket socket=null;
+
+try{
+
+String line="";
+ss = new ServerSocket(1900);    //crează obiectul serversocket
+            System.out.println("astept conexiuni…");
+            socket = ss.accept(); //incepe aşteptarea pe portul 1900
+//în momentul în care un client s-a conectat ss.accept() returnează
+//un socket care identifică conexiunea
+//crează fluxurile de intrare/ieşire
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream()));
+    PrintWriter out = new PrintWriter(
+            new BufferedWriter(new OutputStreamWriter(
+                    socket.getOutputStream())),true);
+    String input = "";
+    Scanner scanner = new Scanner(System.in);
+    while (!input.equals("END")) {
+        out.println("s: " + input);
+        String str = in.readLine();       //primeste mesaj
+        System.out.println(str);         //aşteaptă răspuns
+        System.out.print(">");
+        input = scanner.next();
+    }
+}catch(Exception e){e.printStackTrace();}
+finally{
+ss.close();
+
+if(socket!=null) socket.close();
+
+}
+
+}
+
+}
