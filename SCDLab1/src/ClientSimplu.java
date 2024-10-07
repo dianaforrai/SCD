18d678d03a5e36ce7617492b0254335e49976eea
Diana Forrai Susana <diana.forrai12@yahoo.com>
2024-10-07 13:26:56 +0300

    Server multifir

--- SCDLab1/src/ClientSimplu.java
+++ SCDLab1/src/ClientSimplu.java
@@ -4,19 +4,19 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientSimplu {
-    public static void main(String[] args)throws Exception{
-        Socket socket=null;
+    public static void main(String[] args) throws Exception {
+        Socket socket = null;
         try {
 
 //creare obiect address care identifică adresa serverului
-            InetAddress address =InetAddress.getByName("localhost");
+            InetAddress address = InetAddress.getByName("localhost");
             //se putea utiliza varianta alternativă: InetAddress.getByName("127.0.0.1")
-            socket = new Socket(address,1900);
-            BufferedReader in =new BufferedReader(new InputStreamReader(
+            socket = new Socket(address, 1900);
+            BufferedReader in = new BufferedReader(new InputStreamReader(
                     socket.getInputStream()));
             // Output is automatically flushed  by PrintWriter:
-            PrintWriter out =new PrintWriter(new BufferedWriter(
-                    new OutputStreamWriter(socket.getOutputStream())),true);
+            PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())), true);
             Scanner scanner = new Scanner(System.in);
             String input = "";
             while (!input.equals("END")) {
@@ -36,11 +36,9 @@ public class ClientSimplu {
             } */
             out.println("END");        //trimite mesaj care determină serverul să închidă conexiunea
 
-        }
-
-        catch (Exception ex) {ex.printStackTrace();}
-
-        finally{
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
 
             socket.close();
         }
--- SCDLab1/src/Server.java
+++ SCDLab1/src/Server.java
@@ -5,44 +5,45 @@ import java.util.Scanner;
 
 public class Server {
 
-public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-ServerSocket ss=null;
-Socket socket=null;
+        ServerSocket ss = null;
+        Socket socket = null;
 
-try{
+        try {
 
-String line="";
-ss = new ServerSocket(1900);    //crează obiectul serversocket
+            String line = "";
+            ss = new ServerSocket(1900);    //crează obiectul serversocket
             System.out.println("astept conexiuni…");
             socket = ss.accept(); //incepe aşteptarea pe portul 1900
 //în momentul în care un client s-a conectat ss.accept() returnează
 //un socket care identifică conexiunea
 //crează fluxurile de intrare/ieşire
-    BufferedReader in = new BufferedReader(
-            new InputStreamReader(socket.getInputStream()));
-    PrintWriter out = new PrintWriter(
-            new BufferedWriter(new OutputStreamWriter(
-                    socket.getOutputStream())),true);
-    String input = "";
-    Scanner scanner = new Scanner(System.in);
-    while (!input.equals("END")) {
-        if (!input.isEmpty()) {
-            out.println("s: " + input);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(
+                    new BufferedWriter(new OutputStreamWriter(
+                            socket.getOutputStream())), true);
+            String input = "";
+            Scanner scanner = new Scanner(System.in);
+            while (!input.equals("END")) {
+                if (!input.isEmpty()) {
+                    out.println("s: " + input);
+                }
+                String str = in.readLine();       //primeste mesaj
+                System.out.println(str);         //aşteaptă răspuns
+                System.out.print(">");
+                input = scanner.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ss.close();
+
+            if (socket != null) socket.close();
+
         }
-        String str = in.readLine();       //primeste mesaj
-        System.out.println(str);         //aşteaptă răspuns
-        System.out.print(">");
-        input = scanner.next();
+
     }
-}catch(Exception e){e.printStackTrace();}
-finally{
-ss.close();
-
-if(socket!=null) socket.close();
-
-}
-
-}
 
 }
